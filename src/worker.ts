375b48b00410174cdf1729a83e102fdb0e5b9511
Ty Busby <tybusby@gmail.com>
2025-08-20 23:24:35 -0400

    add logging

--- src/worker.ts
+++ src/worker.ts
@@ -43,7 +43,7 @@ export class NWSProxyWorker extends BasicWorker {
         try {
             return JSON.parse(this.env.ALLOWED_ORIGINS);
         } catch (error) {
-            console.error("Error parsing ALLOWED_ORIGINS", error);
+            this.log.error(`Error parsing ALLOWED_ORIGINS ${this.env.ALLOWED_ORIGINS}`, error);
             return [];
         }
     }
